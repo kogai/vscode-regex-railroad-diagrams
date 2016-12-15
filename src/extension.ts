6e5342b47595bea93fb487521b7981c33674531e
小飼慎一 <kogaishinichi@kogaishinichi-no-MacBook-Pro.local>
2016-12-15 23:12:53 +0900

    lint

--- src/extension.ts
+++ src/extension.ts
@@ -1,116 +1,96 @@
-'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import {window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
+import {window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from "vscode"
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
+  let wordCounter = new WordCounter()
+  let controller = new WordCounterController(wordCounter)
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "wordcount" is now active!');
-    let wordCounter = new WordCounter();
-    let controller = new WordCounterController(wordCounter);
+  let disposable = commands.registerCommand("extension.sayHello", () => {
+    wordCounter.updateWordCount()
+  })
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        wordCounter.updateWordCount();
-        // window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(controller);
-    context.subscriptions.push(wordCounter);
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(controller)
+  context.subscriptions.push(wordCounter)
+  context.subscriptions.push(disposable)
 }
-
-// this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
 
 class WordCounter implements Disposable {
+  private _statusBarItem: StatusBarItem;
 
-    private _statusBarItem: StatusBarItem;
+  public updateWordCount() {
 
-    public updateWordCount() {
-
-        // Create as needed
-        if (!this._statusBarItem) {
-            this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
-        }
-
-        // Get the current text editor
-        let editor = window.activeTextEditor;
-        if (!editor) {
-            this._statusBarItem.hide();
-            return;
-        }
-
-        let doc = editor.document;
-
-        // Only update status if an MarkDown file
-        if (doc.languageId === "markdown") {
-            let wordCount = this._getWordCount(doc);
-
-            // Update the status bar
-            this._statusBarItem.text = wordCount !== 1 ? `$(pencil) ${wordCount} Words` : `$(pencil)`;
-            this._statusBarItem.show();
-        } else {
-            this._statusBarItem.hide();
-        }
+    // Create as needed
+    if (!this._statusBarItem) {
+      this._statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left)
     }
 
-    public _getWordCount(doc: TextDocument): number {
-
-        let docContent = doc.getText();
-
-        // Parse out unwanted whitespace so the split is accurate
-        docContent = docContent.replace(/(< ([^>]+)<)/g, '').replace(/\s+/g, ' ');
-        docContent = docContent.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-        let wordCount = 0;
-        if (docContent != "") {
-            wordCount = docContent.split(" ").length;
-        }
-
-        return wordCount;
+    // Get the current text editor
+    let editor = window.activeTextEditor
+    if (!editor) {
+      this._statusBarItem.hide()
+      return
     }
 
-    dispose() {
-        this._statusBarItem.dispose();
+    let doc = editor.document
+
+    // Only update status if an MarkDown file
+    if (doc.languageId === "markdown") {
+      let wordCount = this._getWordCount(doc)
+
+      // Update the status bar
+      this._statusBarItem.text = wordCount !== 1 ? `$(pencil) ${wordCount} Words` : "$(pencil)"
+      this._statusBarItem.show()
+    } else {
+      this._statusBarItem.hide()
     }
+  }
+
+  public _getWordCount(doc: TextDocument): number {
+
+    let docContent = doc.getText()
+
+    // Parse out unwanted whitespace so the split is accurate
+    docContent = docContent.replace(/(< ([^>]+)<)/g, "").replace(/\s+/g, " ")
+    docContent = docContent.replace(/^\s\s*/, "").replace(/\s\s*$/, "")
+    let wordCount = 0
+    if (docContent != "") {
+      wordCount = docContent.split(" ").length
+    }
+
+    return wordCount
+  }
+
+  dispose() {
+    this._statusBarItem.dispose()
+  }
 }
 
 class WordCounterController implements Disposable {
 
-    private _wordCounter: WordCounter;
-    private _disposable: Disposable;
+  private _wordCounter: WordCounter;
+  private _disposable: Disposable;
 
-    constructor(wordCounter: WordCounter) {
-        this._wordCounter = wordCounter;
-        this._wordCounter.updateWordCount();
+  constructor(wordCounter: WordCounter) {
+    this._wordCounter = wordCounter
+    this._wordCounter.updateWordCount()
 
-        // subscribe to selection change and editor activation events
-        let subscriptions: Disposable[] = [];
-        window.onDidChangeTextEditorSelection(this._onEvent, this, subscriptions);
-        window.onDidChangeActiveTextEditor(this._onEvent, this, subscriptions);
+    // subscribe to selection change and editor activation events
+    let subscriptions: Disposable[] = []
+    window.onDidChangeTextEditorSelection(this._onEvent, this, subscriptions)
+    window.onDidChangeActiveTextEditor(this._onEvent, this, subscriptions)
 
-        // update the counter for the current file
-        this._wordCounter.updateWordCount();
+    // update the counter for the current file
+    this._wordCounter.updateWordCount()
 
-        // create a combined disposable from both event subscriptions
-        this._disposable = Disposable.from(...subscriptions);
-    }
+    // create a combined disposable from both event subscriptions
+    this._disposable = Disposable.from(...subscriptions)
+  }
 
-    dispose() {
-        this._disposable.dispose();
-    }
+  dispose() {
+    this._disposable.dispose()
+  }
 
-    private _onEvent() {
-        this._wordCounter.updateWordCount();
-    }
+  private _onEvent() {
+    this._wordCounter.updateWordCount()
+  }
 }
