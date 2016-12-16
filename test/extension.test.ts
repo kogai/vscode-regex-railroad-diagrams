c5ff0e1b2eaf0286468f90a247a6d247910ef824
小飼慎一 <kogaishinichi@kogaishinichi-no-MacBook-Pro.local>
2016-12-16 10:28:16 +0900

    ci

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -8,7 +8,6 @@ suite("Extension Tests", () => {
 
     // Defines a Mocha unit test
   test("Something 1", () => {
-    assert.equal(true, false)
     assert.equal(-1, [1, 2, 3].indexOf(5))
     assert.equal(-1, [1, 2, 3].indexOf(0))
   })
