39251288d98a389442a09f5af60484f57e515fcd
小飼慎一 <kogai0121@gmail.com>
2017-04-11 13:57:08 +0900

    fix: unintended parse result

--- src/diagram.test.ts
+++ src/diagram.test.ts
@@ -2,25 +2,12 @@ import {deepStrictEqual} from "assert"
 import {extractRegex} from "./diagram"
 
 describe("diagram", () => {
-  // const fixture = `
-  //   exclude: /node_modules(?!\/camelcase)/,
-  //   exclude: /node_modules|src\/components/,
-  //   exclude: /src\/styles/,
-  //   { test: /\.jpg$/, use: { loader: 'url-loader', options: { minetype: 'image/jpeg', limit: 10000 }} },
-  //   { test: /\.png$/, use: { loader: 'url-loader', options: { minetype: 'image/png', limit: 10000 }} },
-  //   { test: /\.svg$/, use: { loader: 'url-loader', options: { minetype: 'image/svg+xml', limit: 10000 }} },
-  //   { test: /\.ttf$/, use: { loader: 'url-loader', options: { minetype: 'application/octet-stream', limit: 10000 }} },
-  //   { test: /\.woff$/, use: { loader: 'url-loader', options: { minetype: 'application/font-woff', limit: 10000 }} },
-  //   { test: /\.woff2$/, use: { loader: 'url-loader', options: { minetype: 'application/font-woff', limit: 10000 }} },
-  //   { test: /\.eot$/, use: { loader: 'url-loader', options: { minetype: 'application/vnd.ms-fontobject', limit: 10000 }} },
-  //   `.split("\n")
-  
   context("extractRegex", () => {
     it("can extract regex string", () => {
       deepStrictEqual(extractRegex("const foo = /foo/g; const"), [{body: "foo", option: "g"}])
     })
-    it("can extract regex string with multiple-flag", () => {
-      deepStrictEqual(extractRegex("const foo = /foo/gim const bar = /bar.?/"), [{body: "foo", option: "gim"}, {body: "bar.?"}])
+    it("can NOT extract regex string with multiple-flag", () => {
+      deepStrictEqual(extractRegex("const foo = /foo/gim const bar = /bar.?/"), [{body: "foo/gim const bar = /bar.?"}])
     })
     it("can extract regex string with single comment", () => {
       deepStrictEqual(extractRegex("const foo = /foo/gim // comment"), [{body: "foo", option: "gim"}])
@@ -36,9 +23,13 @@ describe("diagram", () => {
       const expect = extractRegex(`"/^(props|state)$/",`)
       deepStrictEqual(expect, [{body: "^(props|state)$"}])
     })
-    it.only("exclude: /node_modules(?!\/camelcase)/,", () => {
-      const expect = extractRegex(`exclude: /node_modules(?!\/camelcase)/,`)
-      deepStrictEqual(expect, [{body: "node_modules(?!\/camelcase)"}])
+    it("/some_directory\/foo/", () => {
+      const expect = extractRegex(`/some_directory\/foo/`)
+      deepStrictEqual(expect, [{body: "some_directory\/foo"}])
+    })
+    it("/some_directory\/foo\/bar/", () => {
+      const expect = extractRegex(`/some_directory\/foo\/bar/`)
+      deepStrictEqual(expect, [{body: "some_directory\/foo\/bar"}])
     })
   })
 })
--- src/diagram.ts
+++ src/diagram.ts
@@ -3,25 +3,15 @@ export interface RegexString {
   option?: string
 }
 
-const rx = /\/(.+?)\\{0}\/{0}\/([gimy]*)+/g;
-
-// "\/".match(/\//)
-// ["/", index: 0, input: "/"]
-
-// "\/"
-// "/"
+const rx = /\/(.*[^\/])\/(?!\/)([gimy]*)+/g;
 
 export const extractRegex = (s: string): RegexString[] => {
   const results: RegexString[] = []
   let result: RegExpExecArray | null = null
 
-  console.log(s);
-
   while (result = rx.exec(s)) {
     const body = result[1]
     const option = result[2]
-
-    console.log(result);
 
     if (option.length === 0) {
       results.push({ body })
