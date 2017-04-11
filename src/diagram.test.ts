2bb22013b92aeabd56e9813a26cc7ade22effdb2
小飼慎一 <kogai0121@gmail.com>
2017-04-11 09:27:36 +0900

    Refactor: replace tokenizer to regex

--- src/diagram.test.ts
+++ src/diagram.test.ts
@@ -1,190 +1,44 @@
 import {deepStrictEqual} from "assert"
-import {extractRegex, lexer, tokenize} from "./diagram"
+import {extractRegex} from "./diagram"
 
 describe("diagram", () => {
-  it("can tokenize line", () => {
-    deepStrictEqual(lexer("const foo = /foo/g; const"), [
-      { value: "c", prev: undefined, next: "o", kind: "Extraneous", inRegex: false, offset: 0 },
-      { value: "o", prev: "c", next: "n", kind: "Extraneous", inRegex: false, offset: 1 },
-      { value: "n", prev: "o", next: "s", kind: "Extraneous", inRegex: false, offset: 2 },
-      { value: "s", prev: "n", next: "t", kind: "Extraneous", inRegex: false, offset: 3 },
-      { value: "t", prev: "s", next: " ", kind: "Extraneous", inRegex:  false, offset: 4 },
-      { value: " ", prev: "t", next: "f", kind: "Extraneous", inRegex: false, offset: 5 },
-      { value: "f", prev: " ", next: "o", kind: "Extraneous", inRegex: false, offset: 6 },
-      { value: "o", prev: "f", next: "o", kind: "Extraneous", inRegex: false, offset: 7 },
-      { value: "o", prev: "o", next: " ", kind: "Extraneous", inRegex: false, offset: 8 },
-      { value: " ", prev: "o", next: "=", kind: "Extraneous", inRegex: false, offset: 9 },
-      { value: "=", prev: " ", next: " ", kind: "Extraneous", inRegex: false, offset: 10 },
-      { value: " ", prev: "=", next: "/", kind: "Extraneous", inRegex: false, offset: 11 },
-      { value: "/", prev: " ", next: "f", kind: "RegexLiteralStart", inRegex: true, offset: 12 },
-      { value: "f", prev: "/", next: "o", kind: "RegexLiteralBody", inRegex: true, offset: 13 },
-      { value: "o", prev: "f", next: "o", kind: "RegexLiteralBody", inRegex: true, offset: 14 },
-      { value: "o", prev: "o", next: "/", kind: "RegexLiteralBody", inRegex: true, offset: 15 },
-      { value: "/", prev: "o", next: "g", kind: "RegexLiteralEnd", inRegex: true, offset: 16 },
-      { value: "g", prev: "/", next: ";", kind: "RegexLiteralOption", inRegex: true, offset: 17 },
-      { value: ";", prev: "g", next: " ", kind: "Extraneous", inRegex: false, offset: 18 },
-      { value: " ", prev: ";", next: "c", kind: "Extraneous", inRegex: false, offset: 19 },
-      { value: "c", prev: " ", next: "o", kind: "Extraneous", inRegex: false, offset: 20 },
-      { value: "o", prev: "c", next: "n", kind: "Extraneous", inRegex: false, offset: 21 },
-      { value: "n", prev: "o", next: "s", kind: "Extraneous", inRegex: false, offset: 22 },
-      { value: "s", prev: "n", next: "t", kind: "Extraneous", inRegex: false, offset: 23 },
-      { value: "t", prev: "s", next: undefined, kind: "Extraneous", inRegex: false, offset: 24 }
-    ])
-  })
-
-  it("can tokenize line include backslash", () => {
-    deepStrictEqual(lexer("const foo = /\\/foo/g; const"), [
-      { value: "c", prev: undefined, next: "o", kind: "Extraneous", inRegex: false, offset: 0 },
-      { value: "o", prev: "c", next: "n", kind: "Extraneous", inRegex: false, offset: 1 },
-      { value: "n", prev: "o", next: "s", kind: "Extraneous", inRegex: false, offset: 2 },
-      { value: "s", prev: "n", next: "t", kind: "Extraneous", inRegex: false, offset: 3 },
-      { value: "t", prev: "s", next: " ", kind: "Extraneous", inRegex:  false, offset: 4 },
-      { value: " ", prev: "t", next: "f", kind: "Extraneous", inRegex: false, offset: 5 },
-      { value: "f", prev: " ", next: "o", kind: "Extraneous", inRegex: false, offset: 6 },
-      { value: "o", prev: "f", next: "o", kind: "Extraneous", inRegex: false, offset: 7 },
-      { value: "o", prev: "o", next: " ", kind: "Extraneous", inRegex: false, offset: 8 },
-      { value: " ", prev: "o", next: "=", kind: "Extraneous", inRegex: false, offset: 9 },
-      { value: "=", prev: " ", next: " ", kind: "Extraneous", inRegex: false, offset: 10 },
-      { value: " ", prev: "=", next: "/", kind: "Extraneous", inRegex: false, offset: 11 },
-      { value: "/", prev: " ", next: "\\", kind: "RegexLiteralStart", inRegex: true, offset: 12 },
-      { value: "\\", prev: "/", next: "/", kind: "RegexLiteralBody", inRegex: true, offset: 13 },
-      { value: "/", prev: "\\", next: "f", kind: "RegexLiteralBody", inRegex: true, offset: 14 },
-      { value: "f", prev: "/", next: "o", kind: "RegexLiteralBody", inRegex: true, offset: 15 },
-      { value: "o", prev: "f", next: "o", kind: "RegexLiteralBody", inRegex: true, offset: 16 },
-      { value: "o", prev: "o", next: "/", kind: "RegexLiteralBody", inRegex: true, offset: 17 },
-      { value: "/", prev: "o", next: "g", kind: "RegexLiteralEnd", inRegex: true, offset: 18 },
-      { value: "g", prev: "/", next: ";", kind: "RegexLiteralOption", inRegex: true, offset: 19 },
-      { value: ";", prev: "g", next: " ", kind: "Extraneous", inRegex: false, offset: 20 },
-      { value: " ", prev: ";", next: "c", kind: "Extraneous", inRegex: false, offset: 21 },
-      { value: "c", prev: " ", next: "o", kind: "Extraneous", inRegex: false, offset: 22 },
-      { value: "o", prev: "c", next: "n", kind: "Extraneous", inRegex: false, offset: 23 },
-      { value: "n", prev: "o", next: "s", kind: "Extraneous", inRegex: false, offset: 24 },
-      { value: "s", prev: "n", next: "t", kind: "Extraneous", inRegex: false, offset: 25 },
-      { value: "t", prev: "s", next: undefined, kind: "Extraneous", inRegex: false, offset: 26 },
-    ])
-  })
-
-  it("can tokenize line include single comment", () => {
-    deepStrictEqual(lexer("const foo = /foo/g; // const"), [
-      { value: "c", prev: undefined, next: "o", kind: "Extraneous", inRegex: false, offset: 0 },
-      { value: "o", prev: "c", next: "n", kind: "Extraneous", inRegex: false, offset: 1 },
-      { value: "n", prev: "o", next: "s", kind: "Extraneous", inRegex: false, offset: 2 },
-      { value: "s", prev: "n", next: "t", kind: "Extraneous", inRegex: false, offset: 3 },
-      { value: "t", prev: "s", next: " ", kind: "Extraneous", inRegex:  false, offset: 4 },
-      { value: " ", prev: "t", next: "f", kind: "Extraneous", inRegex: false, offset: 5 },
-      { value: "f", prev: " ", next: "o", kind: "Extraneous", inRegex: false, offset: 6 },
-      { value: "o", prev: "f", next: "o", kind: "Extraneous", inRegex: false, offset: 7 },
-      { value: "o", prev: "o", next: " ", kind: "Extraneous", inRegex: false, offset: 8 },
-      { value: " ", prev: "o", next: "=", kind: "Extraneous", inRegex: false, offset: 9 },
-      { value: "=", prev: " ", next: " ", kind: "Extraneous", inRegex: false, offset: 10 },
-      { value: " ", prev: "=", next: "/", kind: "Extraneous", inRegex: false, offset: 11 },
-      { value: "/", prev: " ", next: "f", kind: "RegexLiteralStart", inRegex: true, offset: 12 },
-      { value: "f", prev: "/", next: "o", kind: "RegexLiteralBody", inRegex: true, offset: 13 },
-      { value: "o", prev: "f", next: "o", kind: "RegexLiteralBody", inRegex: true, offset: 14 },
-      { value: "o", prev: "o", next: "/", kind: "RegexLiteralBody", inRegex: true, offset: 15 },
-      { value: "/", prev: "o", next: "g", kind: "RegexLiteralEnd", inRegex: true, offset: 16 },
-      { value: "g", prev: "/", next: ";", kind: "RegexLiteralOption", inRegex: true, offset: 17 },
-      { value: ";", prev: "g", next: " ", kind: "Extraneous", inRegex: false, offset: 18 },
-      { value: " ", prev: ";", next: "/", kind: "Extraneous", inRegex: false, offset: 19 },
-      { value: "/", prev: " ", next: "/", kind: "Extraneous", inRegex: false, offset: 20 },
-      { value: "/", prev: "/", next: " ", kind: "Extraneous", inRegex: false, offset: 21 },
-      { value: " ", prev: "/", next: "c", kind: "Extraneous", inRegex: false, offset: 22 },
-      { value: "c", prev: " ", next: "o", kind: "Extraneous", inRegex: false, offset: 23 },
-      { value: "o", prev: "c", next: "n", kind: "Extraneous", inRegex: false, offset: 24 },
-      { value: "n", prev: "o", next: "s", kind: "Extraneous", inRegex: false, offset: 25 },
-      { value: "s", prev: "n", next: "t", kind: "Extraneous", inRegex: false, offset: 26 },
-      { value: "t", prev: "s", next: undefined, kind: "Extraneous", inRegex: false, offset: 27 },
-    ])
-  })
-
-  it("can tokenize line include multiline comment", () => {
-    deepStrictEqual(lexer("const foo = /foo/g; /*const*/"), [
-      { value: "c", prev: undefined, next: "o", kind: "Extraneous", inRegex: false, offset: 0 },
-      { value: "o", prev: "c", next: "n", kind: "Extraneous", inRegex: false, offset: 1 },
-      { value: "n", prev: "o", next: "s", kind: "Extraneous", inRegex: false, offset: 2 },
-      { value: "s", prev: "n", next: "t", kind: "Extraneous", inRegex: false, offset: 3 },
-      { value: "t", prev: "s", next: " ", kind: "Extraneous", inRegex:  false, offset: 4 },
-      { value: " ", prev: "t", next: "f", kind: "Extraneous", inRegex: false, offset: 5 },
-      { value: "f", prev: " ", next: "o", kind: "Extraneous", inRegex: false, offset: 6 },
-      { value: "o", prev: "f", next: "o", kind: "Extraneous", inRegex: false, offset: 7 },
-      { value: "o", prev: "o", next: " ", kind: "Extraneous", inRegex: false, offset: 8 },
-      { value: " ", prev: "o", next: "=", kind: "Extraneous", inRegex: false, offset: 9 },
-      { value: "=", prev: " ", next: " ", kind: "Extraneous", inRegex: false, offset: 10 },
-      { value: " ", prev: "=", next: "/", kind: "Extraneous", inRegex: false, offset: 11 },
-      { value: "/", prev: " ", next: "f", kind: "RegexLiteralStart", inRegex: true, offset: 12 },
-      { value: "f", prev: "/", next: "o", kind: "RegexLiteralBody", inRegex: true, offset: 13 },
-      { value: "o", prev: "f", next: "o", kind: "RegexLiteralBody", inRegex: true, offset: 14 },
-      { value: "o", prev: "o", next: "/", kind: "RegexLiteralBody", inRegex: true, offset: 15 },
-      { value: "/", prev: "o", next: "g", kind: "RegexLiteralEnd", inRegex: true, offset: 16 },
-      { value: "g", prev: "/", next: ";", kind: "RegexLiteralOption", inRegex: true, offset: 17 },
-      { value: ";", prev: "g", next: " ", kind: "Extraneous", inRegex: false, offset: 18 },
-      { value: " ", prev: ";", next: "/", kind: "Extraneous", inRegex: false, offset: 19 },
-      { value: "/", prev: " ", next: "*", kind: "Extraneous", inRegex: false, offset: 20 },
-      { value: "*", prev: "/", next: "c", kind: "Extraneous", inRegex: false, offset: 21 },
-      { value: "c", prev: "*", next: "o", kind: "Extraneous", inRegex: false, offset: 22 },
-      { value: "o", prev: "c", next: "n", kind: "Extraneous", inRegex: false, offset: 23 },
-      { value: "n", prev: "o", next: "s", kind: "Extraneous", inRegex: false, offset: 24 },
-      { value: "s", prev: "n", next: "t", kind: "Extraneous", inRegex: false, offset: 25 },
-      { value: "t", prev: "s", next: "*", kind: "Extraneous", inRegex: false, offset: 26 },
-      { value: "*", prev: "t", next: "/", kind: "Extraneous", inRegex: false, offset: 27 },
-      { value: "/", prev: "*", next: undefined, kind: "Extraneous", inRegex: false, offset: 28 },
-    ])
-  })
-
-  it("can grouped by RegExpToken", () => {
-    deepStrictEqual(tokenize([
-      { value: "c", prev: undefined, next: "o", kind: "Extraneous", inRegex: false, offset: 0 },
-      { value: "o", prev: "c", next: "n", kind: "Extraneous", inRegex: false, offset: 1 },
-      { value: "n", prev: "o", next: "s", kind: "Extraneous", inRegex: false, offset: 2 },
-      { value: "s", prev: "n", next: "t", kind: "Extraneous", inRegex: false, offset: 3 },
-      { value: "t", prev: "s", next: " ", kind: "Extraneous", inRegex:  false, offset: 4 },
-      { value: " ", prev: "t", next: "f", kind: "Extraneous", inRegex: false, offset: 5 },
-      { value: "f", prev: " ", next: "o", kind: "Extraneous", inRegex: false, offset: 6 },
-      { value: "o", prev: "f", next: "o", kind: "Extraneous", inRegex: false, offset: 7 },
-      { value: "o", prev: "o", next: " ", kind: "Extraneous", inRegex: false, offset: 8 },
-      { value: " ", prev: "o", next: "=", kind: "Extraneous", inRegex: false, offset: 9 },
-      { value: "=", prev: " ", next: " ", kind: "Extraneous", inRegex: false, offset: 10 },
-      { value: " ", prev: "=", next: "/", kind: "Extraneous", inRegex: false, offset: 11 },
-      { value: "/", prev: " ", next: "f", kind: "RegexLiteralStart", inRegex: true, offset: 12 },
-      { value: "f", prev: "/", next: "o", kind: "RegexLiteralBody", inRegex: true, offset: 13 },
-      { value: "o", prev: "f", next: "o", kind: "RegexLiteralBody", inRegex: true, offset: 14 },
-      { value: "o", prev: "o", next: "/", kind: "RegexLiteralBody", inRegex: true, offset: 15 },
-      { value: "/", prev: "o", next: "g", kind: "RegexLiteralEnd", inRegex: true, offset: 16 },
-      { value: "g", prev: "/", next: ";", kind: "RegexLiteralOption", inRegex: true, offset: 17 },
-      { value: ";", prev: "g", next: " ", kind: "Extraneous", inRegex: false, offset: 18 },
-      { value: " ", prev: ";", next: "/", kind: "Extraneous", inRegex: false, offset: 19 },
-      { value: "/", prev: " ", next: "*", kind: "Extraneous", inRegex: false, offset: 20 },
-      { value: "*", prev: "/", next: "c", kind: "Extraneous", inRegex: false, offset: 21 },
-      { value: "c", prev: "*", next: "o", kind: "Extraneous", inRegex: false, offset: 22 },
-      { value: "o", prev: "c", next: "n", kind: "Extraneous", inRegex: false, offset: 23 },
-      { value: "n", prev: "o", next: "s", kind: "Extraneous", inRegex: false, offset: 24 },
-      { value: "s", prev: "n", next: "t", kind: "Extraneous", inRegex: false, offset: 25 },
-      { value: "t", prev: "s", next: "*", kind: "Extraneous", inRegex: false, offset: 26 },
-      { value: "/", prev: " ", next: "f", kind: "RegexLiteralStart", inRegex: true, offset: 27 },
-      { value: "f", prev: "/", next: "o", kind: "RegexLiteralBody", inRegex: true, offset: 28 },
-      { value: "o", prev: "f", next: "o", kind: "RegexLiteralBody", inRegex: true, offset: 29 },
-      { value: "o", prev: "o", next: "/", kind: "RegexLiteralBody", inRegex: true, offset: 30 },
-      { value: "/", prev: "o", next: "g", kind: "RegexLiteralEnd", inRegex: true, offset: 31 },
-      { value: "g", prev: "/", next: ";", kind: "RegexLiteralOption", inRegex: true, offset: 32 },
-    ]), [
-      {body: "foo", option: "g"},
-      {body: "foo", option: "g"}
-    ])
-  })
-  it("can extract regex string", () => {
-    deepStrictEqual(extractRegex("const foo = /foo/g; const"), [{body: "foo", option: "g"}])
-  })
-  it("can extract regex string with multiple-flag", () => {
-    deepStrictEqual(extractRegex("const foo = /foo/gim const bar = /bar.?/"), [{body: "foo", option: "gim"}, {body: "bar.?"}])
-  })
-  it("can extract regex string with single comment", () => {
-    deepStrictEqual(extractRegex("const foo = /foo/gim // comment"), [{body: "foo", option: "gim"}])
-  })
-  it("can extract with regex string", () => {
-    deepStrictEqual(extractRegex("/foo/g"), [{body: "foo", option: "g"}])
-  })
-  it("/(^ +| {2,}| +$)/", () => {
-    const expect = extractRegex("const parts = text.split(/(^ +| {2,}| +$)/)")
-    deepStrictEqual(expect, [{body: "(^ +| {2,}| +$)"}])
+  // const fixture = `
+  //   exclude: /node_modules(?!\/camelcase)/,
+  //   exclude: /node_modules|src\/components/,
+  //   exclude: /src\/styles/,
+  //   { test: /\.jpg$/, use: { loader: 'url-loader', options: { minetype: 'image/jpeg', limit: 10000 }} },
+  //   { test: /\.png$/, use: { loader: 'url-loader', options: { minetype: 'image/png', limit: 10000 }} },
+  //   { test: /\.svg$/, use: { loader: 'url-loader', options: { minetype: 'image/svg+xml', limit: 10000 }} },
+  //   { test: /\.ttf$/, use: { loader: 'url-loader', options: { minetype: 'application/octet-stream', limit: 10000 }} },
+  //   { test: /\.woff$/, use: { loader: 'url-loader', options: { minetype: 'application/font-woff', limit: 10000 }} },
+  //   { test: /\.woff2$/, use: { loader: 'url-loader', options: { minetype: 'application/font-woff', limit: 10000 }} },
+  //   { test: /\.eot$/, use: { loader: 'url-loader', options: { minetype: 'application/vnd.ms-fontobject', limit: 10000 }} },
+  //   `.split("\n")
+  
+  context("extractRegex", () => {
+    it("can extract regex string", () => {
+      deepStrictEqual(extractRegex("const foo = /foo/g; const"), [{body: "foo", option: "g"}])
+    })
+    it("can extract regex string with multiple-flag", () => {
+      deepStrictEqual(extractRegex("const foo = /foo/gim const bar = /bar.?/"), [{body: "foo", option: "gim"}, {body: "bar.?"}])
+    })
+    it("can extract regex string with single comment", () => {
+      deepStrictEqual(extractRegex("const foo = /foo/gim // comment"), [{body: "foo", option: "gim"}])
+    })
+    it("can extract with regex string", () => {
+      deepStrictEqual(extractRegex("/foo/g"), [{body: "foo", option: "g"}])
+    })
+    it("/(^ +| {2,}| +$)/", () => {
+      const expect = extractRegex("const parts = text.split(/(^ +| {2,}| +$)/)")
+      deepStrictEqual(expect, [{body: "(^ +| {2,}| +$)"}])
+    })
+    it("'/^(props|state)$/',", () => {
+      const expect = extractRegex(`"/^(props|state)$/",`)
+      deepStrictEqual(expect, [{body: "^(props|state)$"}])
+    })
+    it.only("exclude: /node_modules(?!\/camelcase)/,", () => {
+      const expect = extractRegex(`exclude: /node_modules(?!\/camelcase)/,`)
+      deepStrictEqual(expect, [{body: "node_modules(?!\/camelcase)"}])
+    })
   })
 })
